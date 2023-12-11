57b9db9bf23e8b46b9676638eb018b2d68433501
pizza <yulu770520@gmail.com>
2023-12-11 13:42:56 -0500

    calc

--- Calculator/CalculatorTester.java
+++ Calculator/CalculatorTester.java
@@ -11,6 +11,26 @@ public class CalculatorTester
         // create comments to remind yourself 
         // what you need to do
 
-        System.out.println("Hello!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two doubles");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+
+        input.close();
+
+        Calculator calc = new Calculator();
+
+        double sum = calc.sum(a, b);
+        double multi = calc.multiply(a, b);
+        double quotient = calc.divide(a, b);
+        double difference = calc.subtract(a, b);
+
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " - " + b + " = " + difference);
+        System.out.println(a + " * " + b + " = " + multi);
+        System.out.println(a + " / " + b + " = " + quotient);
+        
+
+        
     }
 }
